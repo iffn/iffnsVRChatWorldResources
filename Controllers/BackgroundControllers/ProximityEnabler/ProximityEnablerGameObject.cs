a2014bdeec65474d4c5b52f0420fb1ac527d6d44
iffn <iffn1990@gmail.com>
2022-03-07 14:54:27 +0100

    Added checks to proximity enabler

--- Controllers/BackgroundControllers/ProximityEnabler/ProximityEnablerGameObject.cs
+++ Controllers/BackgroundControllers/ProximityEnabler/ProximityEnablerGameObject.cs
@@ -7,11 +7,22 @@ namespace iffnsStuff.iffnsVRCStuff.BackgroundControllers
 {
     public class ProximityEnablerGameObject : UdonSharpBehaviour
     {
+        [Header("Note: LinkedObject is should not link to this")]
         [SerializeField] GameObject LinkedObject;
         [SerializeField] float ActivationDistance = 5;
 
+        bool setupIncorrect = false;
+
+        private void Start()
+        {
+            setupIncorrect = LinkedObject.transform == transform;
+            if (setupIncorrect) Debug.LogWarning("Warning: ProximityEnablerGameObject of " + transform.name + " is linking to itself.");
+        }
+
         private void Update()
         {
+            if (setupIncorrect) return;
+
             float distance = (transform.position - Networking.LocalPlayer.GetPosition()).magnitude;
 
             LinkedObject.SetActive(distance < ActivationDistance);
--- Controllers/BackgroundControllers/ProximityEnabler/ProximityEnablerUdonSharpScript.cs
+++ Controllers/BackgroundControllers/ProximityEnabler/ProximityEnablerUdonSharpScript.cs
@@ -7,11 +7,22 @@ namespace iffnsStuff.iffnsVRCStuff.BackgroundControllers
 {
     public class ProximityEnablerUdonSharpScript : UdonSharpBehaviour
     {
+        [Header("Note: LinkedBehavior is should not link to this")]
         [SerializeField] UdonSharpBehaviour LinkedBehavior;
         [SerializeField] float ActivationDistance = 5;
 
+        bool setupIncorrect = false;
+
+        private void Start()
+        {
+            setupIncorrect = LinkedBehavior.transform == transform;
+            if (setupIncorrect) Debug.LogWarning("Warning: ProximityEnablerUdonSharpScript of " + transform.name + " is linking to itself.");
+        }
+
         private void Update()
         {
+            if (setupIncorrect) return;
+
             float distance = (transform.position - Networking.LocalPlayer.GetPosition()).magnitude;
 
             LinkedBehavior.enabled = distance < ActivationDistance;
