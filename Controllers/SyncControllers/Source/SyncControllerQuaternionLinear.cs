2a659a56521852ee49cde67895652cd81d2a4567
iffn <iffn1990@gmail.com>
2022-03-09 15:21:01 +0100

    Added Vector3 and Quaternion sync controllers

---
+++ Controllers/SyncControllers/Source/SyncControllerQuaternionLinear.cs
@@ -0,0 +1,75 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+namespace iffnsStuff.iffnsVRCStuff.SyncControllers
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SyncControllerQuaternionLinear : UdonSharpBehaviour
+    {
+        [UdonSynced] Quaternion syncValue = Quaternion.identity;
+        Quaternion lastRecievedValue = Quaternion.identity;
+        Quaternion savedValue = Quaternion.identity;
+        float lastSyncTime = 0;
+        float timeSinceLastSync; //Used to check serialization frequency
+
+        [SerializeField] float SmoothSyncThreshold = 1;
+
+        public void SetValue(Quaternion newValue)
+        {
+            if (!Networking.IsOwner(gameObject)) Networking.SetOwner(player: Networking.LocalPlayer, obj: gameObject);
+
+            syncValue = newValue;
+
+            RequestSerialization();
+        }
+
+        public void SetValueIfChanged(Quaternion newValue, float toleranceAngleDeg)
+        {
+            if (Quaternion.Angle(newValue, syncValue) < toleranceAngleDeg) return;
+
+            SetValue(newValue);
+        }
+
+        public Quaternion GetValue()
+        {
+            if (Networking.IsOwner(gameObject) || Time.time - lastSyncTime > SmoothSyncThreshold) return syncValue;
+
+            return RemapQuaternionWithFloat(
+                inputMin: lastSyncTime,
+                inputMax: lastSyncTime + timeSinceLastSync,
+                outputMin: lastRecievedValue,
+                outputMax: syncValue,
+                inputValue: Time.time);
+        }
+
+        public float GetTimeSinceLastSync()
+        {
+            return Time.time - lastSyncTime;
+        }
+
+        public override void OnDeserialization()
+        {
+            lastRecievedValue = savedValue;
+            savedValue = syncValue;
+
+            timeSinceLastSync = Time.time - lastSyncTime;
+            lastSyncTime = Time.time;
+        }
+
+        public override void OnPostSerialization(SerializationResult result)
+        {
+            timeSinceLastSync = Time.time - lastSyncTime;
+            lastSyncTime = Time.time;
+        }
+
+        public Quaternion RemapQuaternionWithFloat(float inputMin, float inputMax, Quaternion outputMin, Quaternion outputMax, float inputValue)
+        {
+            float t = Mathf.InverseLerp(a: inputMin, b: inputMax, value: inputValue);
+            return Quaternion.Lerp(a: outputMin, b: outputMax, t: t);
+        }
+
+    }
+}
---
+++ Controllers/SyncControllers/Source/SyncControllerVector3Linear.cs
@@ -0,0 +1,74 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common;
+
+namespace iffnsStuff.iffnsVRCStuff.SyncControllers
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SyncControllerVector3Linear : UdonSharpBehaviour
+    {
+        [UdonSynced] Vector3 syncValue = Vector3.zero;
+        Vector3 lastRecievedValue = Vector3.zero;
+        Vector3 savedValue = Vector3.zero;
+        float lastSyncTime = 0;
+        float timeSinceLastSync; //Used to check serialization frequency
+
+        [SerializeField] float SmoothSyncThreshold = 1;
+
+        public void SetValue(Vector3 newValue)
+        {
+            if (!Networking.IsOwner(gameObject)) Networking.SetOwner(player: Networking.LocalPlayer, obj: gameObject);
+
+            syncValue = newValue;
+
+            RequestSerialization();
+        }
+
+        public void SetValueIfChanged(Vector3 newValue, float tolerance)
+        {
+            if ((newValue - syncValue).magnitude < tolerance) return;
+
+            SetValue(newValue);
+        }
+
+        public Vector3 GetValue()
+        {
+            if (Networking.IsOwner(gameObject) || Time.time - lastSyncTime > SmoothSyncThreshold) return syncValue;
+
+            return RemapVector3WithFloat(
+                inputMin: lastSyncTime,
+                inputMax: lastSyncTime + timeSinceLastSync,
+                outputMin: lastRecievedValue,
+                outputMax: syncValue,
+                inputValue: Time.time);
+        }
+
+        public float GetTimeSinceLastSync()
+        {
+            return Time.time - lastSyncTime;
+        }
+
+        public override void OnDeserialization()
+        {
+            lastRecievedValue = savedValue;
+            savedValue = syncValue;
+
+            timeSinceLastSync = Time.time - lastSyncTime;
+            lastSyncTime = Time.time;
+        }
+
+        public override void OnPostSerialization(SerializationResult result)
+        {
+            timeSinceLastSync = Time.time - lastSyncTime;
+            lastSyncTime = Time.time;
+        }
+
+        public Vector3 RemapVector3WithFloat(float inputMin, float inputMax, Vector3 outputMin, Vector3 outputMax, float inputValue)
+        {
+            float t = Mathf.InverseLerp(a: inputMin, b: inputMax, value: inputValue);
+            return Vector3.Lerp(a: outputMin, b: outputMax, t: t);
+        }
+    }
+}
