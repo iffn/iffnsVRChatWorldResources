630f22df8d5584231a6f188a4284e24a6d2890cb
iffn <iffn1990@gmail.com>
2022-04-19 13:22:48 +0200

    Added stuff from iffnsUnityResources

--- Controllers/BackgroundControllers/BugFixing/NanLandFixer/NanLandFixerForPlayer.cs
+++ Controllers/BackgroundControllers/BugFixing/NanLandFixer/NanLandFixerForPlayer.cs
@@ -7,11 +7,15 @@ namespace iffnsStuff.iffnsVRCStuff.BugFixing
 {
     public class NanLandFixerForPlayer : UdonSharpBehaviour
     {
+        //Settings
+        const float playerVelocityMax = 5.5f; //Run and jump with run speed 4 amd jump impulse 3 = 5.1 m/s
+        const float maxVelocityIncreaseFactorPerFixedFrame = 1.2f;
+
+        //Runtime variables
         float lastPlayerVelocity = 0;
         float peakPlayerVelocity = 0;
         Vector3 lastPosition2 = Vector3.zero;
         Vector3 lastPosition = Vector3.zero;
-        const float playerVelocityMax = 5.5f; //Run and jump with run speed 4 amd jump impulse 3 = 5.1 m/s
         VRCPlayerApi LocalPlayer;
 
         public float GetLastPlayerVelocity() { return lastPlayerVelocity; }
@@ -30,14 +34,14 @@ namespace iffnsStuff.iffnsVRCStuff.BugFixing
 
             if (LocalPlayer == null)
             {
-                Debug.LogWarning("Error with NanLandFixer: FixedUpdate was run before Start of this object -> Ignore if once");
+                Debug.LogWarning("Error with NanLandFixer: LocalPlayer not assigned. Likely that FixedUpdate was run before Start of this object -> Ignore if once");
                 return;
             }
 
             Vector3 velocity = LocalPlayer.GetVelocity();
             float velocityMagnitude = velocity.magnitude;
 
-            if (velocityMagnitude > playerVelocityMax && velocityMagnitude > lastPlayerVelocity * 1.2f)
+            if (velocityMagnitude > playerVelocityMax && velocityMagnitude > lastPlayerVelocity * maxVelocityIncreaseFactorPerFixedFrame)
             {
                 Debug.LogWarning(Time.time + "Velocity fixed with velocity = " + velocity + ", position = " + Networking.LocalPlayer.GetPosition() + ", last position = " + lastPosition + ", last position 2 = " + lastPosition2);
 
